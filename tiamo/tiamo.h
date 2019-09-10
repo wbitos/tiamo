d6ff78b190fc5a149e39b98f1c0dee431b7d26d8
wbitos <wbitos@126.com>
2019-09-11 00:40:55 +0800

    Add Exculde

--- tiamo/tiamo.h
+++ tiamo/tiamo.h
@@ -6,7 +6,7 @@
 //  Copyright © 2019 when. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for tiamo.
 FOUNDATION_EXPORT double tiamoVersionNumber;
